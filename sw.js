f5adbede550508085af642ae5ec6bf17b21f79e7
xieyaozhong <117423806+xieyaozhong@users.noreply.github.com>
2026-08-09 00:36:50 +0800

    Refresh offline cache for frequency detector v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='god-way-v1';
+const CACHE='god-way-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
@@ -10,9 +10,9 @@ self.addEventListener('activate',event=>{
 });
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+  event.respondWith(fetch(event.request).then(response=>{
     const copy=response.clone();
     caches.open(CACHE).then(cache=>cache.put(event.request,copy));
     return response;
-  }).catch(()=>caches.match('./index.html'))));
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
 });
